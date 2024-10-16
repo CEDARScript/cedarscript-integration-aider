a8baa6e1224b92cd3cc4332aa4654ccc98ec016c
Elifarley <elifarley@gmail.com>
2024-10-16 15:27:12 -0300

    feat: update package structure and imports

--- src/cedarscript_integration_aider/__init__.py
+++ src/cedarscript_integration_aider/__init__.py
@@ -1,5 +1,12 @@
 from .cedarscript_prompts_g import CEDARScriptScriptPromptsGrammar
+from .cedarscript_prompts_rw import CEDARScriptPromptsRW
+from .cedarscript_prompts_w import CEDARScriptPromptsW
 from ._version import __version__
 
-__all__ = ["CEDARScriptScriptPromptsGrammar", "__version__"]
+__all__ = [
+    "CEDARScriptScriptPromptsGrammar",
+    "CEDARScriptPromptsRW",
+    "CEDARScriptPromptsW",
+    "__version__"
+]
 
