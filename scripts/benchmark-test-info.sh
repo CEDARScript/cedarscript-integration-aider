bd12971583d89b84f561dfbf3c962b05297cce16
Elifarley <elifarley@gmail.com>
2024-10-24 00:49:25 -0300

    Improvements to benchmark_diff_analysis.py

--- scripts/benchmark-test-info.sh
+++ scripts/benchmark-test-info.sh
@@ -26,10 +26,9 @@ benchmark_ls() {
         printf '%2d, %s, %s\n' "$attempts" "$dir_name" "$( aider_results_to_cols "$file" )"
     done < <(find "$benchmark_run_dir" -name '.aider.results.json' -print0 | sort -z)
 
-    printf 'Failed tests: %03.1f%% ( %i / %i )\n' $((100. * failed_test_count / i)) $failed_test_count $i
+    printf 'Success: %03.1f%% ( %i / %i )\n' $((100. * (i-failed_test_count) / i)) $((i-failed_test_count)) $i
     echo "Failed runs : $failures"
 }
 alias benchmark.ls=benchmark_ls "$@"
-test "$#" -eq 0 && exit
-
-benchmark_ls "$@"
+test "$#" -eq 0 || \
+  benchmark_ls "$@"
